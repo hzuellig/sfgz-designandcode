1f68964cbce94f04173ad9b75fe0deb893073be0
MelissaThoma <73747780+MelissaThoma@users.noreply.github.com>
2022-09-15 20:00:42 +0200

    Add files via upload

---
+++ Abgaben/ThomaM/uebungen/tag1/ueb3/sketch.js
@@ -0,0 +1,156 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(0);
+  noStroke();
+
+
+
+  fill(255, 100, 255, 150); 
+  let durchmesser = 100;
+  ellipse(200,200, durchmesser, durchmesser);
+
+
+  durchmesser = durchmesser + 20;
+  ellipse(200,300, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 20;
+  ellipse(200,400, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 20;
+  ellipse(200,500, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 20;
+  ellipse(200,600, durchmesser, durchmesser);
+
+
+  fill(100, 100, 255, 150); 
+  ellipse(400,200, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 20;
+  ellipse(400,300, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 20;
+  ellipse(400,400, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 20;
+  ellipse(400,500, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 20;
+  ellipse(400,600, durchmesser, durchmesser);
+
+
+  fill(50, 100, 0, 150); 
+  ellipse(600,200, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 20;
+  ellipse(600,300, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 20;
+  ellipse(600,400, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 20;
+  ellipse(600,500, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 20;
+  ellipse(600,600, durchmesser, durchmesser);
+
+  fill(100, 0, 50, 150); 
+  ellipse(0,0, durchmesser, durchmesser);
+  
+  durchmesser = durchmesser - 10;
+  ellipse(0,100, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  ellipse(0,200, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  ellipse(0,300, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  ellipse(0,400, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  ellipse(0,500, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  ellipse(0,600, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  ellipse(0,700, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  ellipse(0,800, durchmesser, durchmesser);
+
+  fill(150, 0, 50, 150); 
+  ellipse(800,0, durchmesser, durchmesser);
+  
+  durchmesser = durchmesser + 10;
+  ellipse(800,100, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 10;
+  ellipse(800,200, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 10;
+  ellipse(800,300, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 10;
+  ellipse(800,400, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 10;
+  ellipse(800,500, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 10;
+  ellipse(800,600, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 10;
+  ellipse(800,700, durchmesser, durchmesser);
+
+  durchmesser = durchmesser + 10;
+  ellipse(800,800, durchmesser, durchmesser);
+
+  
+  fill(255, 255, 255, 50) 
+  square(100, 100, 600);
+
+
+  let a = 0;
+  
+  fill(150, 0, 50, 150); 
+  ellipse(0,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+  durchmesser = durchmesser - 10;
+  a = a + 100;
+  ellipse(a,0, durchmesser, durchmesser);
+
+}
