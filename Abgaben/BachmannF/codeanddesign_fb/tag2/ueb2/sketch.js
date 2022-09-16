e5ad48a28d14a9c56866115c9ec22f7a55513c83
Flavia <73498663+flaviabachmann@users.noreply.github.com>
2022-09-16 10:57:06 +0200

    tag 2 uebung 2 alexandra z.

---
+++ Abgaben/BachmannF/codeanddesign_fb/tag2/ueb2/sketch.js
@@ -0,0 +1,60 @@
+let name;
+name = "Alexandra"
+
+//let name="Lex";
+// text(name, mouseX, mouseY); //in die Funktion draw einfügen
+
+let fontRegular;
+let lg_hfiad21 = new Array(); //deklarierung
+lg_hfiad21[0] = "Flavia";
+lg_hfiad21[1] = "Liza";
+lg_hfiad21[2] = "Mirjam";
+lg_hfiad21[3] = "Sario";
+lg_hfiad21[4] = "Cédric";
+lg_hfiad21[5] = "Natascha";
+lg_hfiad21[6] = "Noah";
+lg_hfiad21[7] = "Alexandra";
+lg_hfiad21[8] = "Robin";
+lg_hfiad21[9] = "Katrin";
+lg_hfiad21[10] = "Juri";
+lg_hfiad21[11] = "Melissa";
+lg_hfiad21[12] = "Alexandra";
+
+let key = 0;
+
+function preload() {
+    fontRegular = loadFont('../../libraries/AntiqueOliveStd-Roman.otf');
+
+
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(250, 220, 200)
+
+
+}
+
+function draw() {
+    text(lg_hfiad21[key], mouseX, mouseY);
+    textSize(32);
+    textFont(fontRegular);
+    fill(33, 220, 80);
+    // textSize(45);
+
+    // text(name, mouseX, mouseY);
+}
+
+function mouseClicked(event) {
+    key = key + 1;
+    // Kurzschreibweise n++
+}
+
+
+function keyReleased() {
+    if (key == 's' || key == 'S') {
+        let d = new Date();
+        let now = d.getFullYear() + "" + (d.getMonth() + 1) + "" + d.getDate() + "" + (d.getHours() + 1) + "-" + (d.getMinutes() + 1) + "" + (d.getSeconds() + 1) + "-" + frameCount;
+        saveCanvas(now, 'png');
+    }
+}
