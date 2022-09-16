af39ef8896e02c88425edc9c28723c72a7341f5e
AlexandraZiegler <74136364+AlexandraZiegler@users.noreply.github.com>
2022-09-16 10:55:10 +0200

    Add files via upload

---
+++ Abgaben/ZieglerA/Code-Design/Code-Design/tag1/Uebung2/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    // put setup code here
+    createCanvas(500, 500);
+    background(230, 250, 30);
+}
+
+function draw() {
+    // put drawing code here
+
+    triangle(0, 400, 500, 0);
+
+    triangle(400, 0, 500, 0);
+
+    triangle(500, 00, 0, 500);
+
+    triangle(500, 0, 500, 0);
+
+
+
+}
