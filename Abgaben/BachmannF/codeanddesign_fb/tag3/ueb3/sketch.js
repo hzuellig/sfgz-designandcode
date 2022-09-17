36b3f618108b98c11f0dd5ab561d18469b86db0d
Flavia <73498663+flaviabachmann@users.noreply.github.com>
2022-09-17 11:35:10 +0200

    tag 3 uebung 3

--- Abgaben/BachmannF/codeanddesign_fb/tag3/ueb3/sketch.js
+++ Abgaben/BachmannF/codeanddesign_fb/tag3/ueb3/sketch.js
@@ -1,29 +1,29 @@
-let tracks = new Array(); //hier speichern wir die Mausbewegung
-let max = 100; // eine maximal Begrenzung von Positionen 
+let tracks = new Array();//hier schreiben wir die gespeicherten mausbewegungen rein
+let n = 0;
 
+function preload() {
+
+    let url = 'tracks.json'; //achtung, eventuell pfad anpassen!
+    loadJSON(url, loaded); //die funktion loaded wird aufgerufen, wenn das file fertig geladen ist (callback funktion)
+}
+
+function loaded(data) {
+    tracks = data;
+    console.log(tracks[10]);
+}
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    background(100);
-    frameRate(30); //frameRate verlangsamen
+    background(255);
+    frameRate(10); //frameRate verlangsamen
 }
 
 function draw() {
-    ellipse(mouseX, mouseY, 20, 20)
-
-    let object = {
-
-        x: mouseX,
-        y: mouseY
-
-    }
-
-    tracks.push(object);
-    if (tracks.length > max) {
-        saveJSON(tracks, 'tracks.json');
-        noLoop();
-
-
-    }
+    
+    textSize(400);
+    strokeWeight(2);
+    fill(1,10);
+    text("A", tracks[n].x, tracks[n].y);
+    n++;
 
 }
