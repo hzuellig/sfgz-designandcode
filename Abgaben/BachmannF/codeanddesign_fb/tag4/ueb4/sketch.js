99f0cfd358a26e0835bbc90e27b11032e0be4ae2
Flavia <73498663+flaviabachmann@users.noreply.github.com>
2022-09-24 13:34:22 +0200

    google glass 3

--- Abgaben/BachmannF/codeanddesign_fb/tag4/ueb4/sketch.js
+++ Abgaben/BachmannF/codeanddesign_fb/tag4/ueb4/sketch.js
@@ -1,21 +1,18 @@
 let daten = null
-let farb;
-let farb1;
-let farb2;
-
+let n = 0
 
 function preload() {
-  fontRegular = loadFont('../../libraries/Firm-Regular.ttf');
-
+  fontRegular = loadFont('Firm-Black.otf'); // Font hineinladen
   let url = 'BrowserHistory.json'; //achtung, eventuell pfad anpassen!
   loadJSON(url, loaded); //die funktion loaded wird aufgerufen, wenn das file fertig geladen ist (callback funktion)
 }
 
 function loaded(data) {
   const finaleDaten = [];
-  const historyUrls = data['BrowserHistory']; // Json das geladen ist umbennen zu historyUrls, verschachtelung umgehen
+  const historyUrls = data['Browser History']; // Json das geladen ist umbennen zu historyUrls, verschachtelung umgehen
   const blackList = [
-    /*  'google.com',*/
+    'u.link.ch',
+    //'google.com',
   ];
 
 
@@ -24,7 +21,10 @@ function loaded(data) {
     if (!blackList.includes(domain)) {
       const foundDomain = finaleDaten.find((obj) => obj.url === domain); // ob die URL schon drin ist
       if (!foundDomain) {
-        finaleDaten.push({ url: domain, anzahl: 1 }); // wenn noch nicht in Finale daten einfügen
+        finaleDaten.push({
+          url: domain,
+          anzahl: 1
+        }); // wenn noch nicht in Finale daten einfügen
       } else {
         foundDomain.anzahl += 1; // wenn doch Anzahl +1
       }
@@ -47,17 +47,15 @@ function loaded(data) {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); //mit den JavaScript Variablen könnt ihr die aktuelle Groesse des Fensters abfragen. 
-  // randomSeed(8) -- Erklärt sie uns noch
-  farb = random(255,50);
-  farb1 = random(255);
-  farb2 = random(255);
-  background(farb, farb1, farb2)
-  noLoop();
-
+  createCanvas(window.innerWidth, window.innerHeight);
+  //noLoop();
+  farb=random(255);
+  farb1=random(255);
+  farb2=random(255);
+  background(farb,farb1,farb2);
+  frameRate(4);
 }
-
-let x = 80
+let x = 0
 let y = 0
 // das abstände stimmen
 
@@ -65,27 +63,37 @@ let y = 0
 function draw() {
 
 
-  for (let index = 0; index < daten.length; index++) {
-    let x = map(mouseX, length, width, 0, 255)
-    let y = map(mouseY, length, width, 0, 255)
-    let randomRed = Math.floor(Math.random() * 255),
-      randomGreen = Math.floor(Math.random() * 255),
-      randomBlue = Math.floor(Math.random() * 255);
-
-
-    let randomX = Math.floor(Math.random() * windowWidth),
-      randomY = Math.floor(Math.random() * windowHeight);
-
+  for (let index = 0; index < daten.length; index++) { 
+    let randomX = Math.floor(Math.random() * window.innerWidth);
+    let randomY = Math.floor(Math.random() * window.innerHeight);
     let domain = daten[index] // verkürzen das Daten aus dem Objekt geholt werden
-    let fontsize = (1000 * (domain.prozent / 20))
+    let fontsize = (1500 * (domain.prozent / 80))
+    let c = map(mouseX, 0, width, 0, 255);
+    let c2 = map(mouseX, 0, width, 255, 0);
+    let c3 = map(mouseY, 0, width, 255, 0);
     textSize(fontsize)
     y += fontsize
     textFont(fontRegular);
-    text(domain.url, x, randomY)
-    fill(randomX, randomGreen, randomBlue);
+    text(domain.url, randomX, randomY);
+    fill(c,c2,c3);
+  
+
+
+    /*let eintrag = daten[n].url
+    let position = eintrag.indexOf("a")
+
+    if (position > -1) {
+      fill("blue")
+    } else {
+      fill(200, 2, 20)
+    }*/
+
   }
 
 }
+
+
+
 
 function keyReleased() {
   if (key == 's' || key == 'S') {
@@ -94,3 +102,9 @@ function keyReleased() {
     saveCanvas(now, 'png');
   }
 }
+
+
+
+
+
+
