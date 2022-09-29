7ff8ce339be3e3a2f2a96b1fb258a66252df862c
Flavia <73498663+flaviabachmann@users.noreply.github.com>
2022-09-29 22:03:47 +0200

    Design Changes

--- Abgaben/BachmannF/codeanddesign_fb/tag4/ueb8/sketch.js
+++ Abgaben/BachmannF/codeanddesign_fb/tag4/ueb8/sketch.js
@@ -2,8 +2,10 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
 let datenByWeekday = {};
 let entries = [...Array(days.length).keys()].forEach(weekday => datenByWeekday[weekday] = []);// 0 => [], 1 => [], etc. bis 6 => []
 
+
 function preload() {
     fontRegular = loadFont('../../libraries/Krisha-Regular.otf');
+    fontPixel = loadFont('../../libraries/PressStart2P-Regular.ttf');
 
     let url = 'BrowserHistory.json'; // achtung, eventuell pfad anpassen!
     loadJSON(url, loaded); // die funktion loaded wird aufgerufen, wenn das file fertig geladen ist (callback funktion)
@@ -48,41 +50,50 @@ function loaded(data) {
         .forEach(domains =>
             domains.sort((a, b) => b.anzahl - a.anzahl) // sortieren meiste aufrufe oben tiefste unten); // Anzahl in % umrechnen
         );
-        console.log(datenByWeekday)
+    console.log(datenByWeekday)
+}
+
+function drawGoogle() {
+    background(farb1, farb2, farb3)
+    textFont(fontRegular);
+    textSize(620);
+    fill(farb2, farb1, farb3);
+    textLeading(420);
+    textAlign(CENTER,BASELINE);
+    text(('google\n' + days[currentWeekDay]).toUpperCase(),windowWidth/2,windowHeight/2);
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight); //mit den JavaScript Variablen könnt ihr die aktuelle Groesse des Fensters abfragen. 
-    // randomSeed(8) -- Erklärt sie uns noch
-    //noLoop();
 
     farb1 = random(255);
     farb2 = random(255);
     farb3 = random(255);
 
-    background(farb1, farb2, farb3)
-    frameRate(1);
+    frameRate(10);
 
-    textFont(fontRegular);
-    textSize(650);
-    fill(0, 0, 0);
-    textLeading(420);
-    text(('google\n' + days[currentWeekDay]).toUpperCase(), 50, windowHeight * .5);
+    drawGoogle();
 }
 
 let currentWeekDay = 1;
 
 function draw() {
+
+    let x = map(mouseX, length, width, 0, 1)
+    let y = map(mouseY, length, width, 20, 100)
+
+
     const daten = datenByWeekday[currentWeekDay];
     for (let index = 0; index < daten.length; index++) {
-      let randomX = Math.floor(Math.random() * windowWidth);
-      let randomY = Math.floor(Math.random() * windowHeight);
-      let domain = daten[index];
-      let fontsize = Math.sqrt(domain.prozent) * 50;
-      console.log(fontsize);
-      textSize(fontsize);
-      fill(farb1, farb2, farb3, 70);
-      text(domain.url, randomX, randomY);
+        let randomX = Math.floor(Math.random() * windowWidth);
+        let randomY = Math.floor(Math.random() * windowHeight);
+        let domain = daten[index];
+        let fontsize = Math.sqrt(domain.prozent) * 50;
+        console.log(fontsize);
+        //textFont(fontPixel);
+        textSize(fontsize*x);
+        fill(farb1, farb2, farb3, y);
+        text(domain.url, randomX, randomY);
     }
 }
 
@@ -94,12 +105,10 @@ function keyReleased() {
 }
 
 function mouseReleased() {
+
     currentWeekDay = (currentWeekDay + 1) % 7; // modulo: Rest nach Teilen
     // 0 % 7 == 0
     // 7 % 7 == 0 
-    background(farb1, farb2, farb3);
-    textSize(450);
-    fill(0, 0, 0);
-    textLeading(500 * 1);
-    text(('google\n' + days[currentWeekDay]).toUpperCase(), 100, windowHeight * 0.35);
+    
+    drawGoogle();
 }
